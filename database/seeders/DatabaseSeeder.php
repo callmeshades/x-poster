3ba5d146bebdf055f3e52d01e40a7195a8217cc1
Brandon <brandonnilly@gmail.com>
2025-07-31 18:43:19 -0600

    Seeder & UserFactory Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Brandon',
+            'email' => 'brandon@example.com',
+            'is_admin' => true,
+            'password' => 'password'
         ]);
     }
 }
